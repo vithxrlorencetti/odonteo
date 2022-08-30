b2b2cb830c500e8982f39ae50c3e76a5542bdab4
Rodrigo <rodrigo.facury14@gmail.com>
2022-08-30 15:12:19 -0300

    feat: inicia construção do form principal da tela

---
+++ src/pages/Main/Main.js
@@ -0,0 +1,17 @@
+import './Main.css';
+
+function Main() {
+  return (
+    <main>
+      <form>
+        <input
+          className="form-input"
+          name="payment-amount"
+          type='number'
+        />
+      </form>
+    </main>
+  );
+}
+
+export default Main;
