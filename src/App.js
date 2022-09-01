813e85e01e2db338e1f80d44231328f6c30a3bd2
Rodrigo <rodrigo.facury14@gmail.com>
2022-09-01 17:21:01 -0300

    chore: altera nome da página extract para statement

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
 import './App.css';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Main from './pages/Main/Main';
-import Extract from './pages/Main/Extract/Extract';
+import Statement from './pages/Statement/Statement';
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path='/' element={<Main />} />
-        <Route path='/extract' element={<Extract />} />
+        <Route path='/statement' element={<Statement />} />
       </Routes>
     </BrowserRouter>
   );
