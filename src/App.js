d6f8a49ef4c83f5f100a748826101ccb44f85d05
Rodrigo <rodrigo.facury14@gmail.com>
2022-09-09 17:56:41 -0300

    feat: cria página de login

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Main from './pages/Main/Main';
 import Statement from './pages/Statement/Statement';
+import Login from './pages/Login/Login';
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <Routes>
         <Route path='/' element={<Main />} />
         <Route path='/statement' element={<Statement />} />
+        <Route path='/login' element={<Login />} />
       </Routes>
     </BrowserRouter>
   );
---
+++ src/pages/Login/Login.js
@@ -0,0 +1,71 @@
+import { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import Button from '../../components/Button/Button';
+import Message from '../../components/Message/Message';
+import fetchApi from '../../utils/fetch';
+import { handleChange } from '../../utils/handleChange';
+import showMessage from '../../utils/showMessage';
+
+function Login() {
+  const [loginInformation, setLoginInformation] = useState({});
+  const [message, setMessage] = useState({ show: false, text: '' });
+
+  const navigate = useNavigate();
+
+  async function makeLogin() {
+    const options = {
+      method: 'POST',
+      body: JSON.stringify(loginInformation)
+    }
+
+    const { user, message: apiMessage } = await fetchApi('https://odonteo-backend.herokuapp.com/login', options);
+
+    
+    if (apiMessage === 'Login efetuado com sucesso!') {
+      localStorage.setItem('user', JSON.stringify(user));
+      navigate('/');
+    } else {
+      showMessage(setMessage, apiMessage);
+    }
+  }
+
+  return (
+    <main>
+      { message.show &&
+        <Message>
+          {message.text}
+        </Message>
+      }
+      <form>
+        <label htmlFor='email'>
+          Email:
+          <input
+            className='form-input'
+            id='email'
+            name='email'
+            type='text'
+            onChange={(e) => handleChange(e, setLoginInformation)}
+          />
+        </label>
+        <label htmlFor='password'>
+          Senha:
+          <input
+            className='form-input'
+            id='password'
+            name='password'
+            type='password'
+            onChange={(e) => handleChange(e, setLoginInformation)}
+          />
+        </label>
+        <Button
+          addClassName='form-button'
+          onClickFunction={makeLogin}
+        >
+          Entrar
+        </Button>
+      </form>
+    </main>
+  );
+}
+
+export default Login;
